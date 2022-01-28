d825342bf92883809b5343f71fb37d138312ebc8
Max D <bananafishbone@gmail.com>
2022-01-28 12:37:28 +0300

    initial demoapp state

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="root">
+    <div :class="cssClasses">
+      <router-view
+        name="layout"
+        :title="title"
+        :is-x-small="screen.isXSmall"
+        :is-large="screen.isLarge"
+      >
+        <div class="content dx-swatch-additional">
+          <router-view name="content" />
+        </div>
+        <template #footer>
+          <app-footer />
+        </template>
+      </router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppFooter from "./components/app-footer";
+import { sizes, subscribe, unsubscribe } from "./utils/media-query";
+
+function getScreenSizeInfo() {
+  const screenSizes = sizes();
+
+  return {
+    isXSmall: screenSizes["screen-x-small"],
+    isLarge: screenSizes["screen-large"],
+    cssClasses: Object.keys(screenSizes).filter(cl => screenSizes[cl])
+  };
+}
 
 export default {
-  name: 'App',
+  name: "app",
+  data() {
+    return {
+      title: this.$appInfo.title,
+      screen: getScreenSizeInfo()
+    };
+  },
+  computed: {
+    cssClasses() {
+      return ["app"].concat(this.screen.cssClasses);
+    }
+  },
+  methods: {
+    screenSizeChanged() {
+      this.screen = getScreenSizeInfo();
+    }
+  },
+
+  mounted() {
+    subscribe(this.screenSizeChanged);
+  },
+
+  beforeDestroy() {
+    unsubscribe(this.screenSizeChanged);
+  },
+
   components: {
-    HelloWorld
+    AppFooter
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+html,
+body {
+  margin: 0px;
+  min-height: 100%;
+  height: 100%;
+}
+
+#root {
+  height: 100%;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.app {
+  @import "./themes/generated/variables.base.scss";
+  background-color: darken($base-bg, 5);
+  display: flex;
+  height: 100%;
+  width: 100%;
 }
 </style>
